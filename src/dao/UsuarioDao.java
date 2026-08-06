40f2a56a86dd48cdb560751457dd44d343b28d5e
daos12 <daos_ptc@hotmail.com>
2026-08-05 21:51:25 -0300

    Class UsuarioDAO

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+import controller.ConnectionMySql;
+import model.UsuarioModel;
+import java.sql.Connection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class UsuarioDao {
+
+    private Connection connection;
+
+    public UsuarioDao(Connection connection) {
+        this.connection = new ConnectionMySql().getConnection();
+    }
+
+    public void adicionar(UsuarioModel usuario) {
+        String sql = "INSERT INTO usuario(nome,cpf,email,telefone,nascimento)VALUES(?,?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getCpf());
+            ps.setString(3, usuario.getEmail());
+            ps.setString(4, usuario.getTelefone());
+            //ps.setDate(5, Date.valueOf(usuario.getNascimento()));
+            
+            
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar!");
+            throw new RuntimeException(e);
+        }
+        
+
+    }
+
+}
